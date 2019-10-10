5e351aa7d1565e77a4ae00545025cf7e994a8e4b
huongnt <huongnt@coopy.coopy>
2019-10-10 14:36:02 +0700

    generate embeddings from text or pickle file

--- examples/basic_embedding.py
+++ examples/basic_embedding.py
@@ -6,6 +6,9 @@ generate sentence embeddings for a given list of sentences.
 from sentence_transformers import SentenceTransformer, LoggingHandler
 import numpy as np
 import logging
+import sys
+import time
+import pickle
 
 #### Just some code to print debug information to stdout
 np.set_printoptions(threshold=100)
@@ -14,21 +17,22 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
-#### /print debug information to stdout
-
-
 
 # Load Sentence model (based on BERT) from URL
-model = SentenceTransformer('bert-base-nli-mean-tokens')
+model = SentenceTransformer('bert-large-nli-mean-tokens')
 
-# Embed a list of sentences
-sentences = ['This framework generates embeddings for each input sentence',
-             'Sentences are passed as a list of string.',
-             'The quick brown fox jumps over the lazy dog.']
+if sys.argv[1].endswith('.txt'):
+    sentences = [line.strip() for line in open(sys.argv[1])]
+else:
+    sentences = pickle.load(open(sys.argv[1], 'rb'))
+
+stime = time.time()
 sentence_embeddings = model.encode(sentences)
+sentence_embeddings = np.array(sentence_embeddings)
 
+np.save(sys.argv[2], sentence_embeddings)
 # The result is a list of sentence embeddings as numpy arrays
-for sentence, embedding in zip(sentences, sentence_embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
-    print("")
+#for sentence, embedding in zip(sentences, sentence_embeddings):
+#    print("Sentence:", sentence)
+#    print("Embedding:", embedding)
+#    print("")
